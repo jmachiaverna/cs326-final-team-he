7b2b77d02ee6ac9b0cd1038bbd09795ecd144980
halfu <nickchafy@gmail.com>
2022-11-14 09:20:48 -0500

    Refactored for heroku setup: changed port, added package.json dependencies, updated gitignore, added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+node ./src/development/server.js
--- src/development/server.js
+++ src/development/server.js
@@ -200,8 +200,10 @@ function deleteChirp(chirpDB, id) {
 
 const express = require('express');
 const app = express();
-const port = 3000;
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 /**
  * dummy databases
  */
